f9807f3c9fe0455d1a3ea9c39bbb143c643bdb03
Antonfour28 <duvanrueda0496@gmail.com>
2026-02-13 16:54:54 -0500

    Compled excerce 001

---
+++ LogicConcepts/Esnumero ParOnO/Program.cs
@@ -0,0 +1,31 @@
+﻿
+var numberString = string.Empty;
+
+do
+{
+    Console.WriteLine("Ingrese un número o la plabra Salir para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+
+
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine("El numero es par. ");
+        }
+        else
+        {
+            Console.WriteLine("El numero es impar. ");
+        }
+    }
+    else
+    {
+        Console.WriteLine("El valor ingresado, no es un número válido. Intente nuevamente.");
+    }
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game Over. ");
